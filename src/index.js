5ccbcca0599b2223609a209fbb6ec0d7aa3c613c
HaiNamm <dhtn8xx@gmail.com>
2024-07-18 17:30:33 +0700

    second commit

--- src/index.js
+++ src/index.js
@@ -43,5 +43,5 @@ app.set('views', path.join(__dirname, 'resources', 'views'));
 route(app);
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port: ${port}`);
 });
