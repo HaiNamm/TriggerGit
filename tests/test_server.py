4aba09fd7e733e447bee2fd108f7771f5bee37df
HaiNamm <dhtn8xx@gmail.com>
2024-07-21 15:23:07 +0700

    test new

--- tests/test_server.py
+++ tests/test_server.py
@@ -2,8 +2,8 @@ import pytest
 
 @pytest.fixture
 def test_dummy():
-    assert 2 * 3 == 6
-    assert 2 + 3 == 5
+    assert 2 * 3 == 99
+    assert 2 + 3 == 10
     assert 2 * 2 == 4
     assert 2 - 2 == 0
 
